715278f7254a352abaffc16754281ac640471dfe
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-03-07 18:02:53 -0300

    Aula #07

---
+++ src/app/shared/model/live.model.ts
@@ -0,0 +1,9 @@
+export interface live{
+    id?: string;
+    liveName: string;
+    channelName: string;
+    liveData: string;
+    liveTime: string;
+    liveLink: string;
+    registrationDate: string;
+}
